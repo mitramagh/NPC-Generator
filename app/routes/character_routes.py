0d3da2cfd1d73d8acba3fca88c7d0e6f0d057cf4
Mitra <mit.magh@gmail.com>
2024-06-23 10:54:20 -0400

    Updated Code

--- app/routes/character_routes.py
+++ app/routes/character_routes.py
@@ -3,7 +3,10 @@ from ..db import db
 from ..models.character import Character
 from ..models.greeting import Greeting
 from sqlalchemy import func, union, except_
+from openai import OpenAI
 
+
+client = OpenAI()
 
 bp = Blueprint("characters", __name__, url_prefix="/characters")
 
@@ -43,11 +46,45 @@ def get_characters():
 
 @bp.get("/<char_id>/greetings")
 def get_greetings(char_id):
-    pass
+    character = validate_model(Character, char_id)
+    
+    if not character.greetings:
+        return make_response(jsonify(f"No greetings found for {character.name} "), 201)
+    
+    response = {"Character Name" : character.name,
+                "Greetings" : []}
+    for greeting in character.greetings:
+        response["Greetings"].append({
+            "greeting" : greeting.greeting_text
+        })
+    
+    return jsonify(response)
 
 @bp.post("/<char_id>/generate")
 def add_greetings(char_id):
-    pass
+
+    character_obj = validate_model(Character, char_id)
+    greetings = generate_greetings(character_obj)
+    print(greetings)
+
+    if character_obj.greetings:
+        return make_response(jsonify(f"Greetings already generated for {character_obj.name} "), 201)
+
+    new_greetings = []
+
+    for greeting in greetings:
+        text = greeting[greeting.find(" ")+1:] #Splice the numeric marker off each response
+        new_greeting = Greeting(
+            greeting_text = text.strip("\""), #Strip the response of any extraneous quotes
+            character = character_obj
+        )
+        new_greetings.append(new_greeting)
+    
+    db.session.add_all(new_greetings) #Add all new Greeting records to db
+    db.session.commit()
+
+    return make_response(jsonify(f"Greetings successfully added to {character_obj.name}"), 201)
+
 
 def validate_model(cls,id):
     try:
@@ -63,3 +100,15 @@ def validate_model(cls,id):
 
     response = {"message": f"{cls.__name__} {id} not found"}
     abort(make_response(response, 404))
+    
+def generate_greetings(character):
+    prompt = f"I am writing a video game in the style of a fantasy role playing game. I have an npc named {character.name} who is {character.age} years old. They are a {character.occupation} who has a {character.personality} personality. Please generate a python style list of 10 stock phrases they might use when the main character talks to them"
+
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": prompt}
+    ]
+    )
+    print(completion.choices[0].message.content.split("\n"))
+    return(completion.choices[0].message.content.split("\n"))
